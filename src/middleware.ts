f88ba1b056dd3d7a1f0d1ea9bf2917d24662db6d
Danilo Aguiar <dansilvaagr@hotmail.com>
2024-08-05 17:02:08 -0300

    retirado o middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,56 +1,56 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { decrypt } from "@/lib/crypto";
-import { cookies } from "next/headers";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { decrypt } from "@/lib/crypto";
+// import { cookies } from "next/headers";
 
-const secureCookiePrefix = "securePrefix_";
+// const secureCookiePrefix = "securePrefix_";
 
-export function middleware(request: NextRequest) {
-  const cookieStore = cookies();
-  const encryptedFilialId =
-    cookieStore.get(`${secureCookiePrefix}filialId`)?.value || "";
-  const encryptedClienteId =
-    cookieStore.get(`${secureCookiePrefix}clienteId`)?.value || "";
+// export function middleware(request: NextRequest) {
+//   const cookieStore = cookies();
+//   const encryptedFilialId =
+//     cookieStore.get(`${secureCookiePrefix}filialId`)?.value || "";
+//   const encryptedClienteId =
+//     cookieStore.get(`${secureCookiePrefix}clienteId`)?.value || "";
 
-  const filialId = encryptedFilialId ? decrypt(encryptedFilialId) : null;
-  const clienteId = encryptedClienteId ? decrypt(encryptedClienteId) : null;
+//   const filialId = encryptedFilialId ? decrypt(encryptedFilialId) : null;
+//   const clienteId = encryptedClienteId ? decrypt(encryptedClienteId) : null;
 
-  if (request.nextUrl.pathname.startsWith("/contrate-online")) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+//   if (request.nextUrl.pathname.startsWith("/contrate-online")) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
 
-  if (!filialId && !clienteId && request.nextUrl.pathname !== "/cidades") {
-    return NextResponse.redirect(new URL("/cidades", request.url));
-  }
+//   if (!filialId && !clienteId && request.nextUrl.pathname !== "/cidades") {
+//     return NextResponse.redirect(new URL("/cidades", request.url));
+//   }
 
-  if (filialId && clienteId && request.nextUrl.pathname === "/cidades") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+//   if (filialId && clienteId && request.nextUrl.pathname === "/cidades") {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
 
-  if (
-    !clienteId &&
-    request.nextUrl.pathname === "/contrate-online/cliente-nr/nossos-servicos"
-  ) {
-    return NextResponse.redirect(new URL("/contrate-online", request.url));
-  }
+//   if (
+//     !clienteId &&
+//     request.nextUrl.pathname === "/contrate-online/cliente-nr/nossos-servicos"
+//   ) {
+//     return NextResponse.redirect(new URL("/contrate-online", request.url));
+//   }
 
-  if (clienteId && request.nextUrl.pathname === "/contrate-online") {
-    return NextResponse.redirect(
-      new URL("/contrate-online/cliente-nr/nossos-servicos", request.url)
-    );
-  }
+//   if (clienteId && request.nextUrl.pathname === "/contrate-online") {
+//     return NextResponse.redirect(
+//       new URL("/contrate-online/cliente-nr/nossos-servicos", request.url)
+//     );
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: [
-    "/",
-    "/cidades",
-    "/planos",
-    "/faturas",
-    "/conheca-a-nr/:path*",
-    "/fale-conosco/:path*",
-    "/contrate-online/:path*",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     "/",
+//     "/cidades",
+//     "/planos",
+//     "/faturas",
+//     "/conheca-a-nr/:path*",
+//     "/fale-conosco/:path*",
+//     "/contrate-online/:path*",
+//   ],
+// };
